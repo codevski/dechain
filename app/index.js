b5544ed69da9dabd9457ca35f8c0f738d9f73226
Codevski <1435321+codevski@users.noreply.github.com>
2018-09-11 10:39:47 +1000

    Updated P2P Sync

--- app/index.js
+++ app/index.js
@@ -19,8 +19,11 @@ app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log(`New block added: ${block.toString()}`);
 
+    p2pServer.syncChains();
+
     res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+
 p2pServer.listen();
